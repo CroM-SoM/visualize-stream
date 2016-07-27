a752a3d98979ed78c2fb1d710ae855f4831938b1
mkruijt <maartjekruijt@hotmail.com>
2016-07-27 22:45:16 +0200

    updated json object and information display

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ io.sockets.on('connection', function (socket) {
               if (data.coordinates){
                 if (data.coordinates !== null){
                   //If so then build up some nice json and send out to web sockets
-                  var outputPoint = {"lat": data.coordinates.coordinates[0],"lng": data.coordinates.coordinates[1],"place":data.place.full_name,"location":data.user.location,"text":data.text};
+                  var outputPoint = {"lat": data.coordinates.coordinates[0],"lng": data.coordinates.coordinates[1],"place":data.place.full_name,"location":data.user.location,"text":data.text,"lang":data.user.lang,"time_zone":data.user.time_zone};
 
                   socket.broadcast.emit("twitter-stream", outputPoint);
 
@@ -121,7 +121,7 @@ io.sockets.on('connection', function (socket) {
                     centerLng = centerLng / coords.length;
 
                     // Build json object and broadcast it
-                    var outputPoint = {"lat": centerLat,"lng": centerLng,"place":data.place.full_name,"location":data.user.location,"text":data.text};
+                    var outputPoint = {"lat": centerLat,"lng": centerLng,"place":data.place.full_name,"location":data.user.location,"text":data.text,"lang":data.user.lang,"time_zone":data.user.time_zone};
                     socket.broadcast.emit("twitter-stream", outputPoint);
 
                   }
